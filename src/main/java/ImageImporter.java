d7978512f89f8aefe097f85124d8bc5a488b732b
Yahyawi99 <yassinyahyawi26@gmail.com>
2026-04-08 22:29:09 +0100

    image importer

--- src/main/java/ImageImporter.java
+++ src/main/java/ImageImporter.java
@@ -1,13 +1,23 @@
 import java.util.*;
+import javax.imageio.*;
+import java.awt.image.*;
 import java.io.*;
 
 class ImageImporter implements Importer {
   private static final String PATH = "path";
+  private static final String WIDTH = "width";
+  private static final String HEIGHT = "height";
+  private static final String TYPE = "type";
 
   public Document importFile(final File file) throws IOException {
     final Map<String, String> attributes = new HashMap<>();
 
     attributes.put(PATH, file.getPath());
+
+    final BufferedImage image = ImageIO.read(file);
+    attributes.put(WIDTH, String.valueOf(image.getWidth()));
+    attributes.put(HEIGHT, String.valueOf(image.getHeight()));
+    attributes.put(TYPE, "IMAGE");
 
     return new Document(attributes);
   }
